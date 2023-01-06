d3f0fd0c9904824d3114310279f59cb5702c8302
Alexandra Wong <lex115wong@gmail.com>
2023-01-05 20:06:37 -0800

    Tidy up code

--- public/code.js
+++ public/code.js
@@ -36,49 +36,6 @@ function createTable(data) {
   $("#result").html(result);
 }
 
-// function appendUnicornToTable(unicorn) {
-//   $("#result").append(`
-//     <tr>
-//       <td>${unicorn.name}</td>
-//       <td>${unicorn.dob}</td>
-//       <td>${unicorn.loves}</td>
-//       <td>${unicorn.weight}</td>
-//       <td>${unicorn.gender}</td>
-//       <td>${unicorn.vampires}</td>
-//       <td>${unicorn.vaccinated}</td>
-//     </tr>
-//   `)
-// }
-
-// function tableCreate() {
-//   const body = document.body,
-//         tbl = document.createElement('table');
-//   tbl.style.width = '100px';
-//   tbl.style.border = '1px solid black';
-
-//   for (let i = 0; i < 3; i++) {
-//     const tr = tbl.insertRow();
-//     for (let j = 0; j < 2; j++) {
-//       if (i === 2 && j === 1) {
-//         break;
-//       } else {
-//         const td = tr.insertCell();
-//         td.appendChild(document.createTextNode(`Cell I${i}/J${j}`));
-//         td.style.border = '1px solid black';
-//         if (i === 1 && j === 1) {
-//           td.setAttribute('rowSpan', '2');
-//         }
-//       }
-//     }
-//   }
-//   body.appendChild(tbl);
-// }
-// tableCreate()
-
-// function appendUnicornToTable(unicorn) {
-
-// }
-
 function setup() {
   $("#getByNameBtn").click(function () {
     $.ajax({
@@ -90,35 +47,7 @@ function setup() {
       success: function (data) {
         console.log(data);
         receivedArray = data;
-        // result = "";
-        // result += "<table>"
-        // $("#result").append(`
-        //   <tr>
-        //     <td>${data.name}</td>
-        //     <td>${data.dob}</td>
-        //     <td>${data.loves}</td>
-        //     <td>${data.weight}</td>
-        //     <td>${data.gender}</td>
-        //     <td>${data.vampires}</td>
-        //     <td>${data.vaccinated}</td>
-        //   </tr>
-        // `)
-        // result += "</table>"
-        // result = "";
-        // result += "<table>"
-        // data.map((element) => {
-        //   result +=
-        //     `
-        //   <tr> 
-        //     <td>
-        //       ${element.name}
-        //     </td>
-        //   </tr>
-        //   `
-        // })
-        // result += "</table>"
         $("#result").html(JSON.stringify(data));
-        // appendUnicornToTable(data)
         createTable(data)
         console.log(receivedArray);
       }
@@ -137,22 +66,6 @@ function setup() {
         console.log(data);
         receivedArray = data;
         $("#result").html(JSON.stringify(data));
-        // $("#result").html()
-        // result = "";
-        // result = "<table>"
-        // data.map((aUnicorn) => {
-        //   console.log("aUnicorn", aUnicorn);
-        //   if (aUnicorn["vaccinated"] == true)
-        //     result += `<tr class="red">`
-        //   else
-        //     result += `<tr class="">`
-
-        //   for (var field in aUnicorn) {
-        //     result += `<td>${aUnicorn[field]}</td>`
-        //   }
-        //   result += `</tr>`
-        // })
-        // result += "</table>"
         createTable(data)
         console.log(receivedArray);
       }
@@ -181,10 +94,6 @@ function setup() {
         console.log(receivedArray);
       }
     });
-    // anotherArray = receivedArray.filter((item) => {
-    //   return item.loves.includes("carrots");
-    // })
-    // $("#result").html(JSON.stringify(anotherArray));
   })
 
   $(".submitBtn").click(function () {
